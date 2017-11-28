c79f82321920b6d12482ffb542f9684547c93164
biuroZbywaj <biuro@zbywaj.pl>
2017-11-28 17:02:29 +0100

    commit staged

---
+++ TomaszDrzewieckiCybercom2017/TomaszDrzewieckiCybercom2017/Models/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace TomaszDrzewieckiCybercom2017.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        [Display(Name = "Nazwisko Autora")]
+        public int AuthorId { get; set; }
+        [Display(Name = "Tytuł Książki")]
+        [Required(ErrorMessage = "Podaj Tytuł")]
+        public string Title { get; set; }
+        [Display(Name = "Numer ISBN")]
+        [Required(ErrorMessage = "Podaj numer ISBN")]
+        public int ISBN { get; set; }
+        //data wydania w zakresie 1705-2080 ustawiona także w datepicker'ze!
+        [Required(ErrorMessage = "Podaj liczba z zakresu 1905-2080")]
+        [Range(1705, 2080, ErrorMessage = "tylko wartość z określonego przedziału")]
+        [Display(Name = "Data Wydania")]
+        public int ReleaseDate { get; set; }
+
+    }
+}
---
+++ TomaszDrzewieckiCybercom2017/TomaszDrzewieckiCybercom2017/Scripts/datepicker.js
@@ -0,0 +1,4 @@
+﻿$(function () {
+    $("#datepicker").datepicker({
+        dateFormat: 'yy', changeYear: true, changeMonth: false, yearRange: "1705:2080" });
+});
